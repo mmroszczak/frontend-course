775ab5879d03ae8b8bfc83829c29d95e32854346
Mikolaj Mroszczak <mikolaj.mroszczak2@faculty.ai>
2024-06-19 13:54:35 +0100

    ex1.1 done

--- chapter1/unicafe/src/App.jsx
+++ chapter1/unicafe/src/App.jsx
@@ -6,9 +6,33 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => {
+    setGood(good+1)
+  }
+  
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+  }
+  
+  const handleBad = () => {
+    setBad(bad+1)
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <br/>
+      <button onClick = {handleGood}>good</button>
+      <button onClick = {handleNeutral}>neutral</button>
+      <button onClick = {handleBad}>bad</button>
+      <br/>
+      <h1>statistics</h1>
+      <p>
+        good {good}<br/>
+        neutral {neutral}<br/>
+        bad {bad}<br/>
+      </p>
+
     </div>
   )
 }
