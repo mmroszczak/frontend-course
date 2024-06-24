30f55773ea82e93e08ff0b947048629ac9ccef85
Mikolaj Mroszczak <mikolaj.mroszczak2@faculty.ai>
2024-06-24 10:49:01 +0100

    pre-deploy

---
+++ chapter3/worked_example_FRONTEND/src/components/Note.jsx
@@ -0,0 +1,15 @@
+const Note = ({ note, toggleImportance }) => {
+
+  const label = note.important 
+  ? 'make important' : 'make not important'
+
+    return (
+      <li 
+      className="note">
+        {note.content}
+        <button onClick={toggleImportance}>{label}</button>
+      </li>
+    )
+  }
+
+  export default Note
