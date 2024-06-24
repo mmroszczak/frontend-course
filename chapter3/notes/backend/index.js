db146f05d5d05a7ca5ddd68c46318d5904af1d94
Mikolaj Mroszczak <mikolaj.mroszczak2@faculty.ai>
2024-06-24 15:32:10 +0100

    pre ex 3.15

--- chapter3/notes/backend/index.js
+++ chapter3/notes/backend/index.js
@@ -5,10 +5,9 @@ const mongoose = require('mongoose')
 const app = express()
 const Note  = require('./models/note')
 
-
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
 
 
 app.get('/api/notes', (request, response) => {
@@ -20,9 +19,30 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response) => {
   Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+    if(note){
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }})
+    .catch(error => next(error)) 
+    }
+  )
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
+
   
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -43,11 +63,26 @@ app.post('/api/notes', (request, response) => {
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = request.params.id
-    console.log(notes)
-    notes = notes.filter(note => note.id !== id)
-    console.log(notes)
-    response.status(204).end()
+    Note.findByIdAndDelete(id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+    
   })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
