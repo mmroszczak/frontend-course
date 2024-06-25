9875a4702866b8f9f82efa2ac594d1ca5f5839f4
Mikolaj Mroszczak <mikolaj.mroszczak2@faculty.ai>
2024-06-25 08:39:12 +0100

    ex 4.2 done

---
+++ bloglist/backend/app.js
@@ -0,0 +1,22 @@
+const app = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const cors = require('cors')
+const logger = require('./utils/logger')
+
+
+logger.info(`Connecting to ${config.MONGODB_URI}`)
+mongoose.connect(config.MONGODB_URI)
+.then( () => {
+    logger.info("Connected to MongoDB")
+})
+.catch( () => {
+    logger.error("error connecting to MongoDB", error.message)
+})
+
+app.use(cors())
+app.use(express.json())
+app.use("/api/blogs", blogsRouter)
+
+module.exports = app
---
+++ bloglist/backend/controllers/blogs.js
@@ -0,0 +1,23 @@
+blogsRouter = require('express').Router()
+Blog = require('../models/blog')
+
+
+app.get('/', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+  })
+  
+  app.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+  
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+  })
+
+  module.exports = blogsRouter
