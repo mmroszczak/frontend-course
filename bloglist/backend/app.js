1391544f34572bd75f0a034aad70793b7d91ff74
Mikolaj Mroszczak <mikolaj.mroszczak2@faculty.ai>
2024-06-28 09:16:50 +0100

    periodic commit, save notes app

--- bloglist/backend/app.js
+++ bloglist/backend/app.js
@@ -4,6 +4,8 @@ require('express-async-errors')
 const app = express()
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 const cors = require('cors')
 const logger = require('./utils/logger')
 
@@ -20,5 +22,9 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use("/api/blogs", blogsRouter)
+app.use("/api/users", usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
